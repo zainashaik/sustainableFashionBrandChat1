23abf21115b6628bd79ed16f18de1afb5174dd14
Zaina Shaik <45112851+zainashaik@users.noreply.github.com>
2023-02-12 13:06:32 -0800

    initial fashion chat 1

---
+++ fashionChat1.py
@@ -0,0 +1,47 @@
+from fvalues import F
+from ice.recipe import recipe
+from ice.paper import Paper
+from ice.recipe import recipe
+
+#how to read text from file?
+#sustainability definition
+#company = adidas
+#filename= adidas.txt
+#prompt = 
+
+sustainableDef = "A company is sustainable if they pay their workers a living wage, have supply chain transparency, and are committed to improving their sustainability initiatives. "
+context1 = "Adidas does not pay most of their factory workers a living wage. They have supply chain transparency. They are committed to improving their sustainability initiatives."
+question1 = "Is Adidas sustainable?"
+
+context2 = "Express does pay their factory workers a living wage. They have supply chain transparency. They are committed to improving their sustainability initiatives."
+question2 = "Is Express sustainable?"
+#answer(sustainableDef+context2, question2)
+
+DEFAULT_CONTEXT = sustainableDef + context2
+DEFAULT_QUESTION = question2
+#DEFAULT_CONTEXT = "A company is sustainable if they pay their workers a living wage, have supply chain transparency, and are committed to improving their sustainability initiatives. Adidas does not pay most of their factory workers a living wage. They have supply chain transparency. They are committed to improving their sustainability initiatives."
+#DEFAULT_QUESTION = "Is Adidas sustainable?"
+
+
+def make_qa_prompt(context: str, question: str) -> str:
+    return F(
+        f"""
+Background text: "{context}"
+
+Answer the following question about the background text above:
+
+Question: "{question}"
+Answer: "
+"""
+    ).strip()
+
+
+async def answer(
+    context: str = DEFAULT_CONTEXT, question: str = DEFAULT_QUESTION
+) -> str:
+    prompt = make_qa_prompt(context, question)
+    answer = await recipe.agent().complete(prompt=prompt, stop='"')
+    return answer
+
+
+recipe.main(answer)
